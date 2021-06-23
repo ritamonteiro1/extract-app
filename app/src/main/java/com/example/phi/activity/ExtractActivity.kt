8027382ee88e1e7b268d257105e741ef844b5e43
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-06-23 04:53:26 -0300

    create app

---
+++ app/src/main/java/com/example/phi/activity/ExtractActivity.kt
@@ -0,0 +1,12 @@
+package com.example.phi.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phi.R
+
+class ExtractActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extract)
+    }
+}
---
+++ app/src/main/java/com/example/phi/activity/ReceiptActivity.kt
@@ -0,0 +1,12 @@
+package com.example.phi.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.phi.R
+
+class ReceiptActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_receipt)
+    }
+}
