bd202fa914919cd81b7370d38670143c0c6ed408
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-02 23:09:00 -0300

    add fun findViewsById in ExtractActivity.kt

--- app/src/main/java/com/example/phi/activity/ExtractActivity.kt
+++ app/src/main/java/com/example/phi/activity/ExtractActivity.kt
@@ -2,11 +2,23 @@ package com.example.phi.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 import com.example.phi.R
 
 class ExtractActivity : AppCompatActivity() {
+    private var extractAccountBalanceTextView: TextView? = null
+    private var extractRecyclerView: RecyclerView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_extract)
+        findViewsById()
+    }
+
+    private fun findViewsById() {
+        extractAccountBalanceTextView = findViewById(R.id.extractAccountBalanceTextView)
+        extractRecyclerView = findViewById(R.id.extractRecyclerView)
     }
 }
