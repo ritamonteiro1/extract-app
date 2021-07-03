2a8cd304b897c8794812fe2de5297de687133b78
ritamonteiro <ritaalvesmonteiro@gmail.com>
2021-07-02 23:51:22 -0300

    create interface DataService.kt

--- app/src/main/java/com/example/phi/api/DataService.kt
+++ app/src/main/java/com/example/phi/api/DataService.kt
@@ -1,7 +1,30 @@
 package com.example.phi.api
 
+import com.example.phi.domains.AmountResponse
+import com.example.phi.domains.DetailsExtract
+import com.example.phi.domains.ExtractListResponse
+import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Header
 
 interface DataService {
 
+    @GET("myBalance")
+    fun recoverAmount(
+        @Header(TOKEN) token: String?,
+    ): Call<AmountResponse?>
+
+    @GET("myStatement/:limit/:offset")
+    fun recoverExtract(
+        @Header(TOKEN) token: String?,
+    ): Call<ExtractListResponse?>
+
+    @GET("myStatement/detail/:id/")
+    fun recoverDetailsExtract(
+        @Header(TOKEN) token: String?,
+    ): Call<DetailsExtract?>
+
+    companion object {
+        const val TOKEN: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    }
 }
